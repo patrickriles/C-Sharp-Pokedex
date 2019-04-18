be80a451d27071de7993858379014728a9d92352
bryan sauve <B.C.Sauve@hotmail.com>
2019-04-18 20:56:43 -0300

    Started the API caller class. Successfully called one pokemons data. Need to call all ofthe data down and parse it into either json or find a way to make an objext from the string

---
+++ PokeDex/Http/APIcaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Web.Http.Headers;
+
+
+namespace PokeDex.Http
+{
+    class APIcaller
+    {
+        public async void getData()
+        {
+            // Note: the URI constructor will throw an exception
+            // if the string passed is not a valid URI
+            var uri = new Uri("https://pokeapi.co/api/v2/pokemon/1");
+            var httpClient = new HttpClient();
+
+            // Always catch network exceptions for async methods
+            try
+            {
+                var result = await httpClient.GetStringAsync(uri);
+                Debug.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                // Details in ex.Message and ex.HResult.       
+            }
+
+            // Once your app is done using the HttpClient object call dispose to 
+            // free up system resources (the underlying socket and memory used for the object)
+            httpClient.Dispose();
+        }
+    }
+}
--- PokeDex/MainPage.xaml.cs
+++ PokeDex/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using PokeDex.ViewModel;
 using System.Diagnostics;
+using PokeDex.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,15 +26,19 @@ namespace PokeDex
     public sealed partial class MainPage : Page
     {
         PokemonViewModel pokeVM;
+        APIcaller fetcher;
+        
 
         public MainPage()
         {
             pokeVM = new PokemonViewModel();
+            fetcher = new APIcaller();
             this.InitializeComponent();
         }
 
         private void About_Click(object sender, RoutedEventArgs e)
         {
+            fetcher.getData();
             Debug.Write("About");
         }
     }
